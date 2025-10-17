56a52d975e01df1f68364433b0ee0ef999b0b868
Kurt Frey <kfrey@idmod.org>
2025-10-17 14:58:25 -0700

    Corrections

--- tests/test_experiment_simulations.py
+++ tests/test_experiment_simulations.py
@@ -1,4 +1,3 @@
-import json
 import os
 import pytest
 
@@ -25,6 +24,7 @@ import sys
 
 parent = Path(__file__).resolve().parent
 sys.path.append(str(parent))
+
 import manifest
 import helpers
 
@@ -36,10 +36,10 @@ class TestExperimentSimulations:
     embedded_python_folder = manifest.embedded_python_folder
     original_working_dir = os.getcwd()
     platform = Platform(manifest.container_platform_name, num_retries=0)
+    tags = {"idmtools": "idmtools-automation", "string_tag": "test", "number_tag": 123}
 
     def setup_custom_params(self):
         self.builders = helpers.BuildersCommon
-
 
     def get_sir_experiment(self, case_name) -> Experiment:
         base_task = EMODTask.from_files(config_path=self.builders.config_file,
@@ -54,19 +54,18 @@ class TestExperimentSimulations:
         num_sims = 3
         builder = SimulationBuilder()
         builder.add_sweep_definition(EMODTask.set_parameter_partial("Run_Number"), range(0, num_sims))
-        experiment = Experiment.from_builder(builder, base_task, name=case_name,
-            tags={"idmtools": "idmtools-automation", "string_tag": "test", "number_tag": 123})
+        experiment = Experiment.from_builder(builder, base_task, name=case_name, tags=self.tags)
 
         return experiment
 
-
     @pytest.fixture(autouse=True)
-    def run_every_test(self, request) -> None:
+    def run_every_test(self, request, capsys) -> None:
         # Pre-test
         self.case_name = os.path.basename(__file__) + "_" + request.node.name
         print(f"\n{self.case_name}")
         self.test_folder = helpers.make_test_directory(self.case_name)
         self.setup_custom_params()
+        self.succeeded = False
 
         # Run test
         yield
@@ -74,14 +73,15 @@ class TestExperimentSimulations:
         # Post-test
         os.chdir(self.original_working_dir)
         helpers.close_logger(logger.parent)
-
+        if (self.succeeded):
+            helpers.delete_existing_folder(self.test_folder)
 
     @pytest.mark.container
     def test_mix_tasks_in_experiment(self):
         task = EMODTask.from_files(config_path=self.builders.config_file_basic,
                                    eradication_path=self.builders.eradication_path,
                                    embedded_python_scripts_path=self.embedded_python_folder)
-        task.tags = {"idmtools": "idmtools-automation", "string_tag": "test", "number_tag": 123}
+        task.tags = self.tags
         task.set_parameter("Enable_Immunity", 0)
 
         # User builder to create simulations
@@ -112,10 +112,12 @@ class TestExperimentSimulations:
 
         # Check total simulations 3+1+3
         sims = experiment.simulations
-        assert len(sims)==7
+        assert len(sims) == 7
         assert experiment.succeeded, f"Experiment {experiment.uid} failed.\n"
         print(f"Experiment {experiment.uid} succeeded.")
 
+        self.succeeded = True
+        return None
 
     @pytest.mark.container
     def test_create_suite(self):
@@ -129,6 +131,8 @@ class TestExperimentSimulations:
         suite_type_expected = FileSuite if manifest.container_platform_name == "ContainerPlatform" else CompsSuite
         assert isinstance(got_suite, suite_type_expected)
 
+        self.succeeded = True
+        return None
 
     @pytest.mark.container
     def test_suite_experiment(self):
@@ -149,7 +153,7 @@ class TestExperimentSimulations:
 
         # Test retrieve experiment from suite
         exps = self.platform._get_children_for_platform_item(got_suite)
-        assert len(exps)==1
+        assert len(exps) == 1
         exp = exps[0]
         experiment_type_expected = FileExperiment if manifest.container_platform_name == "ContainerPlatform" else CompsExperiment
         assert isinstance(exp, experiment_type_expected)
@@ -159,11 +163,11 @@ class TestExperimentSimulations:
         comps_exp = self.platform.get_item(item_id=exp.id, item_type=ItemType.EXPERIMENT, raw=True)
         parent = self.platform._get_parent_for_platform_item(comps_exp)
         assert isinstance(parent, suite_type_expected)
-        assert parent.id==suite.uid
+        assert parent.id == suite.uid
 
         # Test retrieve simulations from experiment
         sims = self.platform._get_children_for_platform_item(comps_exp)
-        assert len(sims)==3
+        assert len(sims) == 3
         sim = sims[0]
         simulation_type_expected = FileSimulation if manifest.container_platform_name == "ContainerPlatform" else CompsSimulation
         assert isinstance(sim, simulation_type_expected)
@@ -176,7 +180,7 @@ class TestExperimentSimulations:
 
         # Test retrieve experiment from suite
         exps = self.platform.get_children_by_object(comps_suite)
-        assert len(exps)==1
+        assert len(exps) == 1
         exp = exps[0]
         assert isinstance(exp, Experiment)
         suite.platform.refresh_status(exp)
@@ -187,16 +191,19 @@ class TestExperimentSimulations:
         comps_exp = self.platform.get_item(item_id=exp.uid, item_type=ItemType.EXPERIMENT)
         parent = self.platform.get_parent_by_object(comps_exp)
         assert isinstance(parent, Suite)
-        assert parent.uid==suite.uid
+        assert parent.uid == suite.uid
 
         # Test retrieve simulations from experiment
         sims = self.platform.get_children_by_object(comps_exp)
-        assert len(sims)==3
+        assert len(sims) == 3
         sim = sims[0]
         assert isinstance(sim, Simulation)
         assert sim.parent is not None
 
         assert suite.succeeded, f"Suite {suite.uid} failed.\n"
+
+        self.succeeded = True
+        return None
 
 
 class TestExperimentSimulationsGeneric(TestExperimentSimulations):

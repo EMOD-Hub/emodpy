4dd835ed8cbd79abb77772237c227c80bc6ec718
Kurt Frey <kfrey@idmod.org>
2025-10-17 23:11:38 -0700

    Refactor test file to pytest

--- tests/test_emod_experiment.py
+++ tests/test_emod_experiment.py
@@ -2,11 +2,10 @@ import copy
 import json
 import os
 import pytest
-import unittest
-import time
 
-from idmtools.assets import Asset, AssetCollection
+from idmtools.assets import Asset
 from idmtools.builders import SimulationBuilder
+from idmtools.core import ItemType
 from idmtools.core.platform_factory import Platform
 from idmtools.entities.experiment import Experiment
 from idmtools.entities.simulation import Simulation
@@ -16,49 +15,103 @@ from pathlib import Path
 import sys
 parent = Path(__file__).resolve().parent
 sys.path.append(str(parent))
+
 import manifest
 import helpers
-# add custom_reports to all the tests?
-# especially sweeps
 
 
-class TestEMODExperiment(unittest.TestCase):
+class TestEMODExperiment:
     """
     This tests GENERIC_SIM from emod-common package
     """
-    def setUp(self) -> None:
-        self.num_sim = 2
-        self.num_sim_long = 20
-        self.case_name = os.path.basename(__file__) + "_" + self.__class__.__name__ + "_" + self._testMethodName
-        print(f"\n{self.case_name}")
-        self.setup_custom_params()
-        self.original_working_dir = os.getcwd()
-        self.task: EMODTask
-        self.experiment: Experiment
-        self.platform = Platform(manifest.container_platform_name, num_retries=0)
-        self.test_folder = helpers.make_test_directory(case_name=self.case_name)
+    num_sim = 2
+    num_sim_long = 20
+    original_working_dir = manifest.test_directory_absolute_path
 
     def setup_custom_params(self):
         self.builders = helpers.BuildersCommon
 
-    def tearDown(self) -> None:
-        # Check if the test failed and leave the data in the folder if it did
-        test_result = self.defaultTestResult()
-        if test_result.errors:
-            with open("experiment_location.txt", "w") as f:
-                if self.experiment:
-                    f.write(f"The failed experiment can be viewed at {self.platform.endpoint}/#explore/"
-                            f"Simulations?filters=ExperimentId={self.experiment.uid}")
-                else:
-                    f.write("The experiment was not created.")
-            os.chdir(self.original_working_dir)
-            helpers.close_logger(logger.parent)
-        else:
-            helpers.close_logger(logger.parent)
-            if os.name == "nt":
-                time.sleep(1)  # only needed for windows
-            os.chdir(self.original_working_dir)
+    def singularity_test(self, my_sif_path, embedded_python_scripts_path=None):
+        """
+        Helper function for various singularity tests
+        """
+        base_task = EMODTask.from_defaults(eradication_path=self.builders.eradication_path,
+                                           schema_path=self.builders.schema_path,
+                                           config_builder=self.builders.config_builder,
+                                           demographics_builder=self.builders.demographics_builder,
+                                           embedded_python_scripts_path=embedded_python_scripts_path,
+                                           report_builder=self.builders.reports_builder)
+
+        base_task.set_sif(my_sif_path, platform=self.platform)
+        exp_obj = Experiment.from_task(base_task, self.case_name)
+        exp_obj.run(wait_until_done=True, platform=self.platform)
+        assert exp_obj.succeeded
+
+        sim = exp_obj.simulations[0]
+        files = self.platform.get_files(sim, ["stdout.txt"])
+        stdout = files["stdout.txt"].decode("utf-8")
+        assert "EMOD Disease Transmission Kernel" in stdout
+
+    @pytest.fixture(autouse=True)
+    def run_every_test(self, request) -> None:
+        # Pre-test
+        self.case_name = request.node.name
+        print(f"\n{self.case_name}")
+        os.chdir(self.original_working_dir)
+        self.platform = Platform(manifest.container_platform_name, num_retries=0)
+        self.test_folder = helpers.make_test_directory(self.case_name)  # Moves to failed test directory
+        self.setup_custom_params()
+        self.succeeded = False
+
+        # Run test
+        yield
+
+        # Post-test
+        helpers.close_logger(logger.parent)
+        os.chdir(self.original_working_dir)
+        if (self.succeeded):
             helpers.delete_existing_folder(self.test_folder)
+
+    @pytest.mark.comps
+    def test_experiment_from_task_with_singularity_from_local_file(self):
+        """
+        This checks that if you have a singularity image on your local machine,
+        you can use it to run the simulation
+        """
+        self.platform = Platform(manifest.comps_platform_name)
+        asset_collection_id = "bcf11390-75df-ef11-930c-f0921c167860"  # please update to the latest one
+        out_filename = "dtk_run_rocky_py39.sif"
+
+        targ_sif_file = os.path.join(self.test_folder, out_filename)
+        ret_dict = self.platform.get_files_by_id(asset_collection_id, ItemType.ASSETCOLLECTION, [out_filename])
+        with open(targ_sif_file, 'wb') as fid01:
+            fid01.write(ret_dict[out_filename])
+
+        self.singularity_test(my_sif_path=targ_sif_file)
+
+        self.succeeded = True
+
+    @pytest.mark.comps
+    def test_experiment_from_task_with_singularity(self):
+        """
+        This test checks that you can create a singularity image on Comps and use it to run the simulation
+        """
+        self.platform = Platform(manifest.comps_platform_name)
+        this_sif_path = os.path.join(self.test_folder, "assets.id")
+
+        # use the commented line to create a singularity image on Comps for the first time.
+        sbi = SingularityBuildWorkItem(name="Creating dtk_run_rocky_py39.sif with def file",
+                                       definition_file=os.path.join(self.builders.input_folder, "dtk_run_rocky_py39.def"),
+                                       image_name="dtk_run_rocky_py39.sif")
+        sbi.tags = dict(os="rockylinux", python=3.9)
+        sbi.run(wait_until_done=True, platform=self.platform)
+        assert sbi.succeeded
+
+        # Write asset id to file (needs to be *.id)
+        sbi.asset_collection.to_id_file(this_sif_path)
+        self.singularity_test(my_sif_path=this_sif_path)
+
+        self.succeeded = True
 
     @pytest.mark.container
     def test_experiment_from_task_with_task_from_default_simple(self):
@@ -74,96 +127,11 @@ class TestEMODExperiment(unittest.TestCase):
                                            report_builder=self.builders.reports_builder,
                                            demographics_builder=self.builders.demographics_builder)
 
-        self.experiment = Experiment.from_task(task=base_task,
-                                               name=self.case_name)
+        exp_obj = Experiment.from_task(task=base_task, name=self.case_name)
+        exp_obj.run(wait_until_done=True, platform=self.platform)
+        assert exp_obj.succeeded
 
-        # The last step is to call run() on the ExperimentManager to run the simulations.
-        self.experiment.run(wait_until_done=True, platform=self.platform)
-        self.assertTrue(self.experiment.succeeded)
-
-        sim = self.experiment.simulations[0]
-        files = self.platform.get_files(sim, ["config.json"])
-        config_parameters = json.loads(files["config.json"])['parameters']
-
-    def singularity_test(self, my_sif_path, embedded_python_scripts_path=None):
-        """
-        Helper function for various singularity tests
-        """
-        base_task = EMODTask.from_defaults(eradication_path=self.builders.eradication_path,
-                                           schema_path=self.builders.schema_path,
-                                           config_builder=self.builders.config_builder,
-                                           demographics_builder=self.builders.demographics_builder,
-                                           embedded_python_scripts_path=embedded_python_scripts_path,
-                                           report_builder=self.builders.reports_builder)
-        # to add demographics file this way, you'd have to manually set the Demographics_Filenames
-        # in the base_task.config.parameters
-        # demographics_asset = Asset(self.builders.demographics_file)
-        # base_task.common_assets.add_asset(demographics_asset)
-        # base_task.config.parameters.Demographics_Filenames = [Path(self.builders.demographics_file).name]
-        base_task.set_sif(my_sif_path, platform=self.platform)
-
-        self.experiment = Experiment.from_task(base_task,
-                                               self.case_name)
-
-        self.experiment.run(wait_until_done=True,
-                            platform=self.platform)
-
-        self.assertTrue(self.experiment.succeeded)
-        sim = self.experiment.simulations[0]
-        files = self.platform.get_files(sim, ["stdout.txt"])
-        stdout = files["stdout.txt"].decode("utf-8")
-        self.assertIn("EMOD Disease Transmission Kernel", stdout)
-
-    def download_singularity_ac(self, asset_collection_id, out_filename, output_path):
-        # download sif from comps, currently not working, see issue:
-        # https://github.com/InstituteforDiseaseModeling/idmtools/issues/1574
-        #
-        # self.platform.get_files_by_id(asset_collection_id, ItemType.ASSETCOLLECTION, out_filenames, output_path)
-        # this is a workaround
-        assets = AssetCollection.from_id(asset_collection_id, platform=self.platform).assets
-        for asset in assets:
-            if asset.filename == out_filename:
-                asset.save_as(os.path.join(output_path, out_filename))
-                break
-
-    @pytest.mark.comps
-    def test_experiment_from_task_with_singularity_from_local_file(self):
-        """
-        This checks that if you have a singularity image on your local machine,
-        you can use it to run the simulation
-        """
-        self.platform = Platform(manifest.comps_platform_name)
-        asset_collection_id = "bcf11390-75df-ef11-930c-f0921c167860"  # please update to the latest one
-        out_filename = "dtk_run_rocky_py39.sif"
-        self.download_singularity_ac(asset_collection_id, out_filename, self.test_folder)
-        self.singularity_test(my_sif_path=os.path.join(self.test_folder, out_filename))
-
-    @pytest.mark.comps
-    def test_experiment_from_task_with_singularity(self):
-        """
-        This test checks that you can create a singularity image on Comps and use it to run the simulation
-        """
-
-        def make_sif(my_sif_path):
-            """
-            Creating a singularity image on Comps, only run when the image is not available on Comps.
-            After creating the image, please use the Asset Collection ID of this image in the simulation.
-            """
-            sbi = SingularityBuildWorkItem(name="Creating dtk_run_rocky_py39.sif with def file",
-                                           definition_file=os.path.join(self.builders.input_folder,
-                                                                        "dtk_run_rocky_py39.def"),
-                                           image_name="dtk_run_rocky_py39.sif")
-            sbi.tags = dict(os="rockylinux", python=3.9)
-            sbi.run(wait_until_done=True, platform=self.platform)
-            self.assertTrue(sbi.succeeded)
-            # Write asset id to file (needs to be *.id)
-            sbi.asset_collection.to_id_file(my_sif_path)
-
-        self.platform = Platform(manifest.comps_platform_name)
-        this_sif_path = os.path.join(self.test_folder, "assets.id")
-        # use the commented line to create a singularity image on Comps for the first time.
-        make_sif(this_sif_path)
-        self.singularity_test(this_sif_path)
+        self.succeeded = True
 
     @pytest.mark.container
     def test_experiment_from_task_with_task_from_default_param_custom_cb(self):
@@ -171,10 +139,8 @@ class TestEMODExperiment(unittest.TestCase):
         """
             Test idmtools.entities.experiment.Experiment.from_task() with EMODTask.from_defaults() (set_param_fn=None)
         """
-
         def config_setting(config):
-            if self.builders.config_builder: # when running emod-generic there is no config_builder
-                config = self.builders.config_builder(config)
+            config = self.builders.config_builder(config)
             config.parameters.Simulation_Duration = 7
             config.parameters.Enable_Demographics_Builtin = 1
             return config
@@ -183,15 +149,17 @@ class TestEMODExperiment(unittest.TestCase):
                                            schema_path=self.builders.schema_path,
                                            config_builder=config_setting)
 
-        self.experiment = Experiment.from_task(base_task,
-                                               self.case_name)
-        self.experiment.run(wait_until_done=True, platform=self.platform)
+        exp_obj = Experiment.from_task(base_task, self.case_name)
+        exp_obj.run(wait_until_done=True, platform=self.platform)
 
-        self.assertTrue(self.experiment.succeeded)
-        sim = self.experiment.simulations[0]
+        assert exp_obj.succeeded
+
+        sim = exp_obj.simulations[0]
         files = self.platform.get_files(sim, ["config.json"])
         config_parameters = json.loads(files["config.json"])['parameters']
-        self.assertEqual(config_parameters["Simulation_Duration"], 7)
+        assert config_parameters["Simulation_Duration"] == 7
+
+        self.succeeded = True
 
     @pytest.mark.container
     def test_experiment_from_builder_with_task_from_default(self):
@@ -210,15 +178,16 @@ class TestEMODExperiment(unittest.TestCase):
         builder = SimulationBuilder()
         # Sweep parameter "Run_Number"
         builder.add_sweep_definition(EMODTask.set_parameter_partial("Run_Number"), range(0, self.num_sim))
-        self.experiment = Experiment.from_builder(builder,
-                                                  base_task,
-                                                  name=self.case_name)
-        self.experiment.run(wait_until_done=True, platform=self.platform)
-        self.assertTrue(self.experiment.succeeded)
-        for sim in self.experiment.simulations:
+        exp_obj = Experiment.from_builder(builder, base_task, name=self.case_name)
+        exp_obj.run(wait_until_done=True, platform=self.platform)
+        assert exp_obj.succeeded
+
+        for sim in exp_obj.simulations:
             files = self.platform.get_files(sim, ["config.json"])
             config_parameters = json.loads(files["config.json"])['parameters']
-            self.assertEqual(config_parameters["Run_Number"], sim.tags["Run_Number"])
+            assert config_parameters["Run_Number"] == sim.tags["Run_Number"]
+
+        self.succeeded = True
 
     @pytest.mark.container
     def test_simulations_manual_builder_with_task_from_file(self):
@@ -236,17 +205,16 @@ class TestEMODExperiment(unittest.TestCase):
                                         custom_reports_path=self.builders.custom_reports_file,
                                         embedded_python_scripts_path=manifest.embedded_python_folder)
 
-        self.experiment = Experiment(name=self.case_name)
+        exp_obj = Experiment(name=self.case_name)
 
         base_sim = Simulation(task=base_task)
         for i in range(self.num_sim_long):
             sim = copy.deepcopy(base_sim)
             sim.task.config["Run_Number"] = i
-            self.experiment.simulations.append(sim)
+            exp_obj.simulations.append(sim)
 
-        with self.assertRaises(DuplicatedAssetError) as context:
-            self.experiment.run(wait_until_done=True, platform=self.platform)
-        self.assertTrue("demographics.json" in str(context.exception))
+        with pytest.raises(DuplicatedAssetError):
+            exp_obj.run(wait_until_done=True, platform=self.platform)
 
         #  uncomment when fixed
         # self.assertTrue(self.experiment.succeeded)
@@ -255,6 +223,8 @@ class TestEMODExperiment(unittest.TestCase):
         #     files = self.platform.get_files(sim, ["config.json"])
         #     config_parameters = json.loads(files["config.json"])['parameters']
         #     self.assertEqual(config_parameters["Run_Number"], i)
+
+        self.succeeded = True
 
     @pytest.mark.container
     def test_simulations_manual_builder_with_task_from_file_workaround(self):
@@ -275,25 +245,27 @@ class TestEMODExperiment(unittest.TestCase):
                                         config_path="config.json",
                                         embedded_python_scripts_path=manifest.embedded_python_folder)
 
-        self.experiment = Experiment(name=self.case_name)
+        exp_obj = Experiment(name=self.case_name)
         base_sim = Simulation(task=base_task)
         for i in range(self.num_sim_long):
             sim = copy.deepcopy(base_sim)
             sim.task.config["Run_Number"] = i
-            self.experiment.simulations.append(sim)
+            exp_obj.simulations.append(sim)
 
-        self.experiment.simulations[0].task.common_assets.add_asset(Asset(self.builders.demographics_file))
-        self.experiment.run(wait_until_done=True, platform=self.platform)
+        exp_obj.simulations[0].task.common_assets.add_asset(Asset(self.builders.demographics_file))
+        exp_obj.run(wait_until_done=True, platform=self.platform)
 
-        self.assertTrue(self.experiment.succeeded)
+        assert exp_obj.succeeded
 
         run_numbers = []
-        for sim in self.experiment.simulations:
+        for sim in exp_obj.simulations:
             files = self.platform.get_files(sim, ["config.json"])
             config_parameters = json.loads(files["config.json"])['parameters']
             run_numbers.append(config_parameters["Run_Number"])
 
-        self.assertEqual(set(run_numbers), set(list(range(self.num_sim_long))))
+        assert set(run_numbers) == set(list(range(self.num_sim_long)))
+
+        self.succeeded = True
 
     @pytest.mark.container
     def test_experiment_from_builder_with_task_from_file(self):
@@ -307,16 +279,17 @@ class TestEMODExperiment(unittest.TestCase):
         base_task.set_parameter("Enable_Interventions", 0)
         builder = SimulationBuilder()
         builder.add_sweep_definition(EMODTask.set_parameter_partial("Run_Number"), range(0, self.num_sim_long))
-        self.experiment = Experiment.from_builder(builder,
-                                                  base_task,
-                                                  name=self.case_name)
-        self.experiment.run(wait_until_done=True, platform=self.platform)
+        exp_obj = Experiment.from_builder(builder, base_task, name=self.case_name)
+        exp_obj.run(wait_until_done=True, platform=self.platform)
 
-        self.assertTrue(self.experiment.succeeded)
-        for sim in self.experiment.simulations:
+        assert exp_obj.succeeded
+
+        for sim in exp_obj.simulations:
             files = self.platform.get_files(sim, ["config.json"])
             config_parameters = json.loads(files["config.json"])['parameters']
-            self.assertEqual(config_parameters["Run_Number"], sim.tags["Run_Number"])
+            assert config_parameters["Run_Number"] == sim.tags["Run_Number"]
+
+        self.succeeded = True
 
 
 class TestEMODExperimentGeneric(TestEMODExperiment):
@@ -324,10 +297,4 @@ class TestEMODExperimentGeneric(TestEMODExperiment):
     This tests GENERIC_SIM from emod-generic package
     """
     def setup_custom_params(self):
-        self.builders = helpers.BuildersCommon
-
-
-if __name__ == "__main__":
-    import unittest
-
-    unittest.main()
+        self.builders = helpers.BuildersGeneric

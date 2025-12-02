64e5ab49d7ee57acd9c0a147b8f97d513dbd0ac7
Clark Kirkman IV <clark.kirkman@gatesfoundation.org>
2025-12-02 14:45:04 -0800

    review updates, removing some consts to malaria and a default age distribution that was discussed to be unneded

--- emodpy/demographics/demographics.py
+++ emodpy/demographics/demographics.py
@@ -6,16 +6,14 @@ from emodpy.utils.distributions import UniformDistribution
 
 
 class Demographics(EMODAPIDemographics):
-    PYROGENIC_THRESHOLD = 'PYROGENIC_THRESHOLD'
-    CYTOKINE_KILLING = 'CYTOKINE_KILLING'
+    # TODO: move to emodpy-malaria as only it uses these for innate immune distributions
+    # PYROGENIC_THRESHOLD = 'PYROGENIC_THRESHOLD'
+    # CYTOKINE_KILLING = 'CYTOKINE_KILLING'
 
     def __init__(self, nodes: List[Node], default_node: Node, idref: str = "Gridded world grump2.5arcmin"):
         # Forces emodpy-layer Demographics instantiation to use Node object-route for default node . Cannot use
         # the old self.raw dict representation of a default node.
         super().__init__(nodes=nodes, default_node=default_node, idref=idref)
-        if default_node is None:
-            age_distribution = UniformDistribution(uniform_min=0, uniform_max=18250)
-            self.set_age_distribution(distribution=age_distribution)
 
     @property
     def raw(self):

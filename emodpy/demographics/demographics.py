db06c2aa247785232a9f6ada2bc6e3056641f19a
Clark Kirkman IV <clark.kirkman@gatesfoundation.org>
2025-12-04 23:45:21 -0800

    updating pass-through args for emodpy Demographics class to mirror underlying emod-api Demographics class

--- emodpy/demographics/demographics.py
+++ emodpy/demographics/demographics.py
@@ -2,18 +2,14 @@ from typing import List
 
 from emod_api.demographics.demographics import Demographics as EMODAPIDemographics
 from emod_api.demographics.node import Node
-from emodpy.utils.distributions import UniformDistribution
-
 
 class Demographics(EMODAPIDemographics):
-    # TODO: move to emodpy-malaria as only it uses these for innate immune distributions
-    # PYROGENIC_THRESHOLD = 'PYROGENIC_THRESHOLD'
-    # CYTOKINE_KILLING = 'CYTOKINE_KILLING'
 
-    def __init__(self, nodes: List[Node], default_node: Node, idref: str = "Gridded world grump2.5arcmin"):
-        # Forces emodpy-layer Demographics instantiation to use Node object-route for default node . Cannot use
-        # the old self.raw dict representation of a default node.
-        super().__init__(nodes=nodes, default_node=default_node, idref=idref)
+    def __init__(self, nodes: List[Node], default_node: Node = None, idref: str = None, set_defaults: bool = True):
+        super().__init__(nodes=nodes, default_node=default_node, idref=idref, set_defaults=set_defaults)
+
+    # Forces emodpy-layer Demographics instantiation to use Node object-route for default node . Cannot use
+    # the old self.raw dict representation of a default node.
 
     @property
     def raw(self):
